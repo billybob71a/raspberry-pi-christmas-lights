e226de219a73990d36bd57b07625c65600bde994
Peter Yung <yungpet@gmail.com>
2017-12-08 00:17:29 -0700

    Making lights blink every 5 minutes

--- relay.py
+++ relay.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 import time
 from RPi import GPIO
+import re
+
 
 def init(a):
     pass
+
+def setpins():
+    pins = [4]
+    GPIO.setmode(GPIO.BCM)
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
 
 def main():
     pins = [4]
@@ -12,20 +21,41 @@ def main():
 
     for pin in pins:
         GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(pin, GPIO.LOW)
 
     try:
         # setup initial pin state
-        init(pins)
+        a = []
+        c = False
+        d = []
+        while True:
+            init(pins)
+            b = False
+            a = time.ctime().split(' ')
+            d = a[4].split(':')
+            int_d = int(d[1])
+	    if int_d % 5 == 0 :
+            #if re.match(r'^19\:00', a[4]):
+                b = True
+                c = True
+            while b:
+                for i in range(2):
+                    for pin in pins:
+                        GPIO.output(pin, GPIO.LOW)
+                        time.sleep(0.5)
+                        GPIO.output(pin, GPIO.HIGH)
+                        time.sleep(0.5)
+                a = time.ctime().split(' ')
+		d = a[4].split(':')
+                int_d = int(d[1])
+                if int_d % 5 <> 0:
+                #if re.match(r'^19\:15', a[4]):
+                    b = False 
 
-        for i in range(2):
-            for pin in pins:
-                GPIO.output(pin, GPIO.LOW)
-                time.sleep(0.5)
-                GPIO.output(pin, GPIO.HIGH)
-                time.sleep(0.5)
-
-        GPIO.cleanup()
+            if c == True:
+                GPIO.cleanup()
+                c = False
+                setpins()
 
     except KeyboardInterrupt:
         GPIO.cleanup()
